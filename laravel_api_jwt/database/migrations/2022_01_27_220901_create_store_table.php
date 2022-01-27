6b9aa72358cd0ccca42a4e00edf97ad6817c45fd
Fabricio Ferreira <fabroide@gmail.com>
2022-01-27 19:20:54 -0300

    #migration-store

---
+++ laravel_api_jwt/database/migrations/2022_01_27_220901_create_store_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_fantasya',100);
+            $table->string('cnpj', 24)->nullable();
+            $table->string('street', 100)->nullable();
+            $table->string('number', 10)->nullable();
+            $table->string('complement', 30);
+            $table->string('city', 20)->nullable();
+            $table->string('state', 20)->nullable();
+            $table->string('cellphone', 20)->nullable();
+            $table->string('site', 20)->nullable();
+            $table->string('email', 30)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store');
+    }
+}
